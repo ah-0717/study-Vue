a55dfda2d6996e6b7d64abd6ab1c21d9aab2f5d3
ah <ah@dummy.com>
2020-01-01 07:22:31 +0900

    slot

--- 03_vue-video/04_understanding/01_start-cli/src/App.vue
+++ 03_vue-video/04_understanding/01_start-cli/src/App.vue
@@ -2,10 +2,24 @@
   <div>
     <!-- componentsに指定したkey名がパスカルケースならば、HTMLをケバブケースでも記述できる -->
     <!-- key名がケバブの時はHTMLをパスカルケースで記述はできない -->
-    <like-header></like-header>
-    <h2>トータルのいいね数{{number}}</h2>
+    <like-header> 
+      <!-- 名前つきslot -->
+      <!-- slotプロパティを使用すれば子コンポーネントの値を使用できる v-slotは#に置き換わる -->
+      <template #:[title]="slotProps" >
+        <h2 >こんにちは</h2>
+        <p>{{slotProps}}</p>
+      </template>
+      <h3>初めまして</h3>
+      <template v-slot:number>
+        <h2>トータルのいいね数{{number}}</h2>
+      </template>
+    </like-header>
+    <!-- デフォルトスロットのしかない場合のみ下記の様な書き方ができる 省略（#）はできない -->
+    <like-header v-slot="slotProps">
+      <p>{{slotProps}}</p>
+    </like-header>
     <!-- キャメルケースでもかける -->
-    <!-- <LikeNumber :totalNumber="number" @my-click="number = $event"></LikeNumber> -->
+    <!-- <LikeNumber :totalNumber="number" @my-click="number = $event"></LikeNumber> --> <!-- $eventにemitの値が入る -->
     <LikeNumber :totalNumber="number" @my-click="incrementNumber"></LikeNumber>
     <!-- ケバブケースでもかける プロパティはこちらを推奨 -->
     <LikeNumber :total-number="number"></LikeNumber>
@@ -21,7 +35,9 @@ export default {
   // babelをしようしているのでES6もOK
   data() {
     return {
-      number: 10
+      number: 10,
+      // 動的な名前
+      title: 'title'
     }
   },
   components: {
--- 03_vue-video/04_understanding/01_start-cli/src/components/LikeHeader.vue
+++ 03_vue-video/04_understanding/01_start-cli/src/components/LikeHeader.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>トータルのいいね数</h1>
+  <div>
+    <!-- 親からHTMLのテンプレートを受け取る -->
+    <slot name="title" :user="user" text="text">
+      <!-- 指定されないとデフォルトが適応される -->
+      <h2>デフォルトのタイトル</h2>
+    </slot>
+    <!-- デフォルトslot 名前つき以外がここに入る v-slot:default name="default"の定義と同等 -->
+    <slot test="test"></slot>
+    <hr>
+    <slot name="number"></slot>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        firstName: 'aaa',
+        lastName: 'bbb',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* soltで適応されるHTMLにスタイルを当てられる、親側で定義しても当たるその場合は親が勝つ */
+  h2 {
+    color: red;
+  }
+</style>
