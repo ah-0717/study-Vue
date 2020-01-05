aef344675f8099459bbbd14aae618f847f64ad95
ah <ah@dummy.com>
2020-01-05 23:22:51 +0900

    カスタムディレクティブ

--- 03_vue-video/04_understanding/01_start-cli/src/components/Home.vue
+++ 03_vue-video/04_understanding/01_start-cli/src/components/Home.vue
@@ -1,3 +1,36 @@
 <template>
-  <p>Home</p>
+  <!-- :の後に引数を取れる、ただし、一つだけ -->
+  <!-- 修飾詞も登録できる -->
+  <p v-border2:solid.round.shadow="{width: '5px', color: 'red'}">{{home}}</p>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      home: 'Home'
+    }
+  },
+  // ローカル登録
+  directives: {
+    border2(el, binding) {
+      // 注意：ディレクティブでthisは使えない
+      // this.home = 'aaaaa'
+
+      // DOMを直接操作
+      // el.style.border = 'solid black 2px'
+      el.style.borderWidth = binding.value.width
+      el.style.borderColor = binding.value.color
+      // 引数を受け取る
+      el.style.borderStyle = binding.arg
+      // 修飾詞の判定
+      if (binding.modifiers.round) {
+        el.style.borderRadius = '0.5rem'
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.26)'
+      }
+    }
+  }
+}
+</script>
--- 03_vue-video/04_understanding/01_start-cli/src/main.js
+++ 03_vue-video/04_understanding/01_start-cli/src/main.js
@@ -6,8 +6,46 @@ import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = true
 
+// コンポーネント
 // グローバル登録
-Vue.component('LikeNumber', LikeNumber);
+Vue.component('LikeNumber', LikeNumber)
+
+// カスタムディレクティブ
+// グローバル登録
+// Vue.directive('border', {
+//   bind(el, binding, vnode) {
+//     // ディレクティブが初めて対象の対象の要素に紐づいた時
+//   },
+//   inserted(el, binding, vnode) {
+//     // 親Node（リアル）に挿入された時
+//   },
+//   update(el, binding, vnode, oldVnode) {
+//     // コンポーネントが更新される度。子コンポーネントが変更される前
+//   },
+//   componentUpdated(el, binding, vnode, oldVnode) {
+//     // コンポーネントが更新される度。子コンポーネントが変更された後
+//   },
+//   unbind(el, binding, vnode) {
+//     // ディレクティブが紐づいている要素から取り除かれた時
+//   },
+// });
+
+// bindとupdateの省略記法
+Vue.directive('border', (el ,binding) => {
+  // DOMを直接操作
+  // el.style.border = 'solid black 2px'
+  el.style.borderWidth = binding.value.width
+  el.style.borderColor = binding.value.color
+  // 引数を受け取る
+  el.style.borderStyle = binding.arg
+  // 修飾詞の判定
+  if (binding.modifiers.round) {
+    el.style.borderRadius = '0.5rem'
+  }
+  if (binding.modifiers.shadow) {
+    el.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.26)'
+  }
+});
 
 new Vue({
   // コンポーネントのオブジェクトを与えてる
