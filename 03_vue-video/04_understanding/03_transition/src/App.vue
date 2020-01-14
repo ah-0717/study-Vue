eed6a477bb78980c85b6c138f50f29a6a91107d8
ah <ah@dummy.com>
2020-01-14 18:57:32 +0900

    トラジション　最初のコミット

---
+++ 03_vue-video/04_understanding/03_transition/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <!-- nameをつけないとデフォルトでname="v" -->
+    <transition name="fade">
+      <p v-if="show">Hello</p>
+    </transition>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* ６つのトラジションクラス */
+.fade-enter {
+  /* 現れる時の最初の状態 */
+}
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+}
+.fade-enter-to {
+  /* 現れる時の最後の状態 */
+}
+.fade-leave {
+  /* 消える時の最初の状態 */
+}
+.fade-leave-active {
+  /* 消える時のトランジションの状態 */
+}
+.fade-leave-to {
+  /* 消える時の最後の状態 */
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
