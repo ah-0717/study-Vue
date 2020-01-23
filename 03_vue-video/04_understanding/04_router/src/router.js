8f10193cf05ccd4ac3c2c6474a4000d2506d8783
ah-0717 <ah.recruit.1991@gmail.com>
2020-01-23 19:33:38 +0900

    history mode

--- 03_vue-video/04_understanding/04_router/src/router.js
+++ 03_vue-video/04_understanding/04_router/src/router.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Router from './vue-router'
+import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Users from './views/Users.vue'
 
@@ -8,6 +8,7 @@ Vue.use(Router)
 
 
 export default new Router({
+  mode: 'history', // hash, historyの選択
   // urlとコンポーネントのマッピング
   routes: [
     {path: '/', component: Home},
