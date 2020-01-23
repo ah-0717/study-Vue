7985cb6f6113798a2a227590d12f5e5cab78aed8
ah <ah.recruit.1991@gmail.com>
2020-01-24 00:47:00 +0900

    router-link

--- 03_vue-video/04_understanding/04_router/src/App.vue
+++ 03_vue-video/04_understanding/04_router/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div style="with: 700px margin: auto; padding-top: 50px;">
+    <nav >
+      <router-link to="/">Home</router-link>
+      <router-link to="/users">Users</router-link>
+    </nav>
     <!-- router-viewがurlに応じて動的にコンポーネントが切り替わる -->
     <router-view></router-view>
   </div>
